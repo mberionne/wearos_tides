eea7aec8e9f88793312fc6f6eb270ef40c0647ae
mberionne <mberionne@gmail.com>
2024-03-13 08:12:03 +0000

    First working version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/foundation.dart';
@@ -22,17 +23,26 @@ enum AppState {
 }
 
 class TideData {
-  TideData({required this.station, required this.heights});
+  TideData(
+      {required this.station, required this.heights, required this.extremes});
   final String station;
-  final List<double> heights;
+  final SplayTreeMap<double, double> heights;
+  final Map<double, double> extremes;
 
   factory TideData.fromJson(Map<String, dynamic> data) {
+    // This function does not perform any operation, but simply
+    // extracts the values of interest from the JSON.
     final station = data['station']; // dynamic
-    List<double> heights = []; // dynamic
+    SplayTreeMap<double, double> heights = SplayTreeMap();
     for (final height in data['heights']) {
-      heights.add(height['height']);
+      heights[height['dt']] = height['height'];
     }
-    return TideData(station: station ?? "", heights: heights);
+    Map<double, double> extremes = {};
+    for (final extreme in data['extremes']) {
+      extremes[extreme['dt']] = extreme['height'];
+    }
+    return TideData(
+        station: station ?? "", heights: heights, extremes: extremes);
   }
 }
 
@@ -51,7 +61,7 @@ class CoordinateConv {
     assert(hour >= 0 && hour <= 24);
     assert(value >= minValue && value <= maxValue);
     return Offset(width / 24 * hour,
-        -(height / (maxValue - minValue) * (minValue + value)));
+        -(height / (maxValue - minValue) * (value - minValue)));
   }
 }
 
@@ -92,6 +102,12 @@ class _MainPageState extends State<MainPage> {
   Location location = Location();
   TideData? _tideData;
 
+  @override
+  void initState() {
+    super.initState();
+    _mainFlow();
+  }
+
   void _mainFlow() async {
     try {
       // TO DO: Fetch the location only if the cache is invalid.
@@ -106,7 +122,6 @@ class _MainPageState extends State<MainPage> {
       }
       _moveTo(AppState.parsingData);
       TideData tideData = _parseBody(body);
-      _debugMessage = "Heights: ${tideData.heights}";
       _moveTo(AppState.ready, tideData);
     } on TimeoutException catch (_) {
       _moveTo(AppState.errorHttpTimeout);
@@ -136,8 +151,12 @@ class _MainPageState extends State<MainPage> {
     });
   }
 
-  double metersToFeet(double m) {
+  double _metersToFeet(double m) {
     return m * 3.28084;
+  }
+
+  double _epochToHour(double ms) {
+    return ms / 3600;
   }
 
   String _getCurrentDate() {
@@ -166,15 +185,25 @@ class _MainPageState extends State<MainPage> {
       throw Exception("Invalid result received ($result)");
     }
     TideData tideData = TideData.fromJson(map);
-    return TideData(
-        station: tideData.station,
-        heights: tideData.heights.map((val) => metersToFeet(val)).toList());
+
+    // Normalize the timestamp in hours and the values in feet.
+    double minTimestamp = tideData.heights.keys.reduce(min);
+    SplayTreeMap<double, double> h = SplayTreeMap<double, double>.fromIterable(
+        tideData.heights.entries,
+        key: (e) => _epochToHour(e.key - minTimestamp),
+        value: (e) => _metersToFeet(e.value));
+    h.removeWhere((key, value) => key > 24);
+    Map<double, double> e = tideData.extremes.map((key, value) =>
+        MapEntry(_epochToHour(key - minTimestamp), _metersToFeet(value)));
+
+    return TideData(station: tideData.station, heights: h, extremes: e);
   }
 
   Uri _composeUri(LocationData locationData) {
     return Uri.parse("https://www.worldtides.info/api/v3?"
         "heights&days=1&date=today&datum=CD&"
-        "lat={$locationData.latitude}&lon={$locationData.longitude}&"
+        "extremes&"
+        "lat=${locationData.latitude}&lon=${locationData.longitude}&"
         "step=3600&"
         "key=8280c866-8a82-44e8-8943-c542836f15af");
   }
@@ -193,8 +222,8 @@ class _MainPageState extends State<MainPage> {
     // Hardcode location for testing on web.
     if (defaultTargetPlatform != TargetPlatform.android) {
       return LocationData.fromMap(<String, double>{
-        'latitude': 32.7157,
-        'longitude': -117.1611,
+        'latitude': 33.768321,
+        'longitude': -118.195617,
       });
     }
 
@@ -226,12 +255,6 @@ class _MainPageState extends State<MainPage> {
           children: showState(),
         ),
       ),
-      // TO DO: to be removed - Only used for testing
-      floatingActionButton: FloatingActionButton(
-        onPressed: _mainFlow,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
@@ -272,10 +295,6 @@ class _MainPageState extends State<MainPage> {
         break;
       case AppState.ready:
         widgets = <Widget>[
-          const Text(
-            "Ready",
-            style: TextStyle(fontSize: 20, color: Colors.white),
-          ),
           SizedBox(
               width: MediaQuery.of(context).size.width * 0.8,
               height: MediaQuery.of(context).size.height * 0.8,
@@ -302,8 +321,15 @@ class TidePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    double maxValue = tideData.heights.reduce(max).ceil().toDouble();
-    double minValue = tideData.heights.reduce(min).floor().toDouble();
+    double maxValue = tideData.heights.values.reduce(max).ceil().toDouble();
+    double minValue = tideData.heights.values.reduce(min).floor().toDouble();
+
+    var paintAxes = Paint()..color = Colors.black;
+    var paintTides = Paint()..color = Colors.blue.shade400;
+    var paintBackground = Paint()..color = Colors.grey.shade100;
+    var paintCurrentTime = Paint()
+      ..color = Colors.red.shade200
+      ..strokeWidth = 2;
 
     // Draw main rectangle
     canvas.drawRRect(
@@ -314,7 +340,7 @@ class TidePainter extends CustomPainter {
               height: size.height),
           const Radius.circular(5),
         ),
-        Paint()..color = Colors.grey.shade100);
+        paintBackground);
 
     // Values for the chart
     const double margin = 10;
@@ -325,14 +351,18 @@ class TidePainter extends CustomPainter {
         width: size.width - (2 * margin),
         height: size.height - (2 * margin));
 
-    var paintAxes = Paint()..color = Colors.black;
     // Draw X axis
-    canvas.drawLine(zero, zero + conv.convert(24, 0), paintAxes);
-    for (double i = 0; i < 24; i++) {
-      Offset t = conv.convert(i, 0);
+    canvas.drawLine(zero, zero + conv.convert(24, minValue), paintAxes);
+    for (double hour in tideData.heights.keys) {
+      // Skip entries that are not round
+      if (hour != hour.toInt()) {
+        continue;
+      }
+      Offset t = conv.convert(hour, minValue);
       canvas.drawLine(zero + t, zero + t + const Offset(0, 2), paintAxes);
-      if (i > 0 && i % 2 == 0) {
-        writeText(canvas, i.toString(), zero + t + const Offset(-2, 2));
+      if (hour > 0 && hour.toInt().isEven) {
+        _writeText(
+            canvas, hour.toString(), zero + t + const Offset(0, margin / 2));
       }
     }
     // Draw Y axis
@@ -340,40 +370,56 @@ class TidePainter extends CustomPainter {
     for (double i = minValue; i <= maxValue; i++) {
       Offset t = conv.convert(0, i);
       canvas.drawLine(zero + t, zero + t + const Offset(-2, 0), paintAxes);
-      writeText(canvas, i.toString(), zero + t + const Offset(-margin, -4));
+      _writeText(canvas, i.toString(), zero + t + const Offset(-margin / 2, 0));
     }
 
     // Draw polygon with tides
     var points = <Offset>[];
-    int minHour = 0;
-    int maxHour = 0;
     points.add(zero + conv.convert(0, minValue));
-    for (int i = 0; i <= 24 && i < tideData.heights.length; i++) {
-      points.add(zero + conv.convert(i.toDouble(), tideData.heights[i]));
-      if (tideData.heights[i] < tideData.heights[minHour]) {
-        minHour = i;
-      } else if (tideData.heights[i] > tideData.heights[maxHour]) {
-        maxHour = i;
-      }
+    for (var entry in tideData.heights.entries) {
+      points.add(zero + conv.convert(entry.key, entry.value));
     }
     points.add(zero + conv.convert(24, minValue));
     Path path = Path();
     path.addPolygon(points, true);
-    var paintTides = Paint()..color = Colors.blue.shade400;
     canvas.drawPath(path, paintTides);
 
     // Write min and max values
-    double v = tideData.heights[maxHour];
-    writeText(canvas, v.toString(),
-        zero + conv.convert(maxHour.toDouble(), v) + const Offset(0, -10));
+    for (var e in tideData.extremes.entries) {
+      String hour = e.key.truncate().toString();
+      String minute = ((e.key - e.key.truncate()) * 60)
+          .truncate()
+          .toString()
+          .padLeft(2, "0");
+      _writeText(canvas, "$hour:$minute",
+          zero + conv.convert(e.key, e.value) + const Offset(0, -10),
+          size: 14.0);
+    }
+
+    // Draw station name
+    _writeText(
+        canvas, tideData.station, zero + conv.convert(12, maxValue - 0.3),
+        size: 14.0);
+
+    // Draw line of current time
+    final now = DateTime.now();
+    double currentHour = now.hour + (now.minute / 60);
+    canvas.drawLine(zero + conv.convert(currentHour, minValue),
+        zero + conv.convert(currentHour, maxValue), paintCurrentTime);
   }
 
-  void writeText(Canvas canvas, String text, Offset offset) {
+  void _writeText(Canvas canvas, String text, Offset offset,
+      {double size = 8.0}) {
     TextSpan span = TextSpan(
-        style: const TextStyle(color: Colors.black, fontSize: 8.0), text: text);
-    TextPainter tp = TextPainter(text: span, textDirection: TextDirection.ltr);
+        style: TextStyle(
+            color: Colors.black, fontSize: size, fontWeight: FontWeight.bold),
+        text: text);
+    TextPainter tp = TextPainter(
+        text: span,
+        textDirection: TextDirection.ltr,
+        textAlign: TextAlign.center);
     tp.layout();
-    tp.paint(canvas, offset);
+    tp.paint(canvas, offset + Offset(-tp.width / 2, -tp.height / 2));
   }
 
   @override
